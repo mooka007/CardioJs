016b345650e918e7cef08baa00accb3dd2a23139
Mooka <47282413+mooka007@users.noreply.github.com>
2022-10-06 12:30:46 +0100

    remove readme
    remove readme

---
+++ js-cardio/Level1/public-index.js
@@ -0,0 +1,68 @@
+// <<<<<<< HEAD
+// CHALLENGE 1: REVERSE A STRING
+// Return a string in reverse
+// ex. reverseString('hello') === 'olleh'
+
+function reverseString(str) {
+    
+    // a Short Example :
+    // =================
+    // CONTRIBUTED SOLUTION
+    // if(str === ""){
+    //   return str 
+    // }else{
+    //   return reverseString(str.substr(1)) + str[0]
+    // }
+    // .... BY <Your-Name>
+    // https://github.com/<Your-Username>
+    // ==================
+  
+}
+
+// CHALLENGE 2: VALIDATE A PALINDROME
+// Return true if palindrome and false if not
+// ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
+
+function isPalindrome(str) {
+  
+  
+}
+
+// CHALLENGE 3: REVERSE AN INTEGER
+// Return an integer in reverse
+// ex. reverseInt(521) === 125
+
+function reverseInt(int) {
+   
+}
+
+// CHALLENGE 4: CAPITALIZE LETTERS
+// Return a string with the first letter of every word capitalized
+// ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
+function capitalizeLetters(str) {
+  
+    
+}
+
+// CHALLENGE 5: MAX CHARACTER
+// Return the character that is most common in a string
+// ex. maxCharacter('javascript') == 'a'
+function maxCharacter(str) {
+ 
+}
+
+//Solution by Naomi Sharp
+//Fixes bug in above solution for which the string "socks" would not work. This is because
+//the char "s" is the last one in the array sortedStr so the code to store maxChar does not run.
+
+function maxCharacter(str) {
+ 
+  }
+
+// CHALLENGE 6: FIZZBUZZ
+// Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
+function fizzBuzz() {
+ 
+}
+
+
